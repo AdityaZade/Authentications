1c6376b804ff42b0621096765654e97c6652f1d7
Aditya <adityazade9970@gmail.com>
2020-07-18 06:16:09 +0530

    Level 4 salting and hashing

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ const express =require("express");
 const bodyParser=require("body-parser")
 const mongoose=require("mongoose")
 const ejs=require("ejs")
-const md5=require("md5")
+const bcrypt=require("bcrypt");
+const saltRounds=10
+
+// level-3
+// const md5=require("md5")
 
 // level-1
 // var encrypt = require('mongoose-encryption');
@@ -35,45 +39,53 @@ app.get("/",function(req,res){
   res.render("home");
 })
 
+app.get("/register",function(req,res){
+  res.render("register");
+});
+
+  app.post("/register",function(req,res){
+
+  bcrypt.hash(req.body.password,saltRounds, function(err, hash) {
+    const newUser = new User({
+      email:req.body.username,
+      password:hash
+    });
+
+    newUser.save(function(err){
+      if(err){
+        res.send(err);
+      }else{
+        res.render("secrets");
+      }
+    });
+});
+});
+
 app.get("/login",function(req,res){
   res.render("login");
 })
 
 app.post("/login",function(req,res){
   const username=req.body.username;
-  const password=md5(req.body.password);
+  const password=req.body.password
 
   User.findOne({email:username}, function(err, foundUser){
     if(err){
       res.send(err);
     }else{
       if(foundUser){
-        if(foundUser.password === password){
-          res.render("secrets");
-        }
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+
+       if(result === true){
+         res.render("secrets")
+       }
+});
       }
     }
   });
 });
 
-app.get("/register",function(req,res){
-  res.render("register");
-})
 
-app.post("/register",function(req,res){
-  const newUser = new User({
-    email:req.body.username,
-    password:md5(req.body.password)
-  });
-
-  newUser.save(function(err){
-    if(err){
-      res.send(err);
-    }else{
-      res.render("secrets");
-    }
-  })
-});
 app.listen(3000,function(){
   console.log("Server is started at port 3000");
 });
