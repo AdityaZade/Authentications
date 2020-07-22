1f88e540a5938a309e99c4f879dc0f1f326cb66f
Aditya <adityazade9970@gmail.com>
2020-07-22 07:08:34 +0530

    LEvel 6 authentication Googl API added

--- app.js
+++ app.js
@@ -21,6 +21,10 @@ const passportLocalMongoose=require("passport-local-mongoose")
 // level-1
 // var encrypt = require('mongoose-encryption');
 
+//level-6
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate')
+
 const app =express();
 
 app.use(express.static("public"));
@@ -39,12 +43,28 @@ app.use(passport.session());
 //level-5 !
 
 
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL:"https://www.googleapis.com/oauth2/v3/userinfo"
+
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile)
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
 mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true,  useUnifiedTopology: true });
 mongoose.set('useCreateIndex', true);
 
 const userSchema=new mongoose.Schema({
   email:String,
-  password:String
+  password:String,
+  googleId:String
 });
 
 // level-1
@@ -53,17 +73,33 @@ const userSchema=new mongoose.Schema({
 
 //level 5
 userSchema.plugin(passportLocalMongoose);
+//level-6
+userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model("User",userSchema);
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
-
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
 
 
 app.get("/",function(req,res){
   res.render("home");
 })
+app.get("/auth/google",
+  passport.authenticate("google", { scope: ['profile'] }));
+
+app.get("/auth/google/secrets",
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
+  });
 
 app.get("/register",function(req,res){
   res.render("register");
